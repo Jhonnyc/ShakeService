48ace54467fab3f9e90e89dba3a1dabb72c7b3aa
Yoni Kalangan <yoni.kalangan@gmail.com>
2017-10-02 08:49:09 +0300

    * Added TAG for sample activity

--- app/src/main/java/com/yonikal/shakeservice/SampleActivity.java
+++ app/src/main/java/com/yonikal/shakeservice/SampleActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class SampleActivity extends AppCompatActivity {
 
+    private static final String TAG = SampleActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
